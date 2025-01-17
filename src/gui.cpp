f9861286aee27546784269e672c541fd0f90a701
TheDraphix <davwes44@gmail.com>
2025-01-17 17:37:09 +1100

    Added wxWidget Submodule

---
+++ src/gui.cpp
@@ -0,0 +1,8 @@
+#include <wx/wx.h>
+#include "gui.hpp"
+
+MainFrame::MainFrame()
+	: wxFrame(NULL, wxID_ANY, "Spyro DOTD Save Editor")
+{
+	
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
 #include <cstdint>
 #include <cstring>
+#include <wx/wx.h>
 
 #include "uuid.hpp"
 #include "byte_utils.hpp"
 #include "save_data.hpp"
+#include "gui.hpp"
 
-int main()
+class Main : public wxApp
 {
-	char input[32];
-	
-	std::cout << "INFILE: ";
-	std::cin >> input;
-	save_data::GameSave save(input);
-	
-	memcpy(save.slot[0].spawn_point_uuid, uuid::spawn::valley_of_avalar[0], 4*sizeof(uint32_t));
-	
-	std::cout << "OUTFILE: ";
-	std::cin >> input;
-	save.Write(input);
-	
-	return 0;
+	public:
+		virtual bool OnInit();
+};
+wxIMPLEMENT_APP(Main);
+
+bool Main::OnInit()
+{
+	MainFrame *frame = new MainFrame();
+	frame->Show(true);
+	return true;
 }
